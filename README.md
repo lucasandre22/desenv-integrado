4831fa10a8f9ed598f65210f17a8857e0a5df29e
lucasandre22 <lucas.andre22@yahoo.com>
2022-09-09 16:02:11 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## Trabalho de Desenvolvimento Integrado de Sistemas
+
+###To compile:  
+Java 8  
+[Spring Tools 4 for Eclipse](https://spring.io/tools)  
+Import Project  
+mvn install
---
+++ desenv-integrado/src/main/java/com/integrado/DesenvIntegradoApplication.java
@@ -0,0 +1,13 @@
+package com.integrado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesenvIntegradoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesenvIntegradoApplication.class, args);
+	}
+
+}
